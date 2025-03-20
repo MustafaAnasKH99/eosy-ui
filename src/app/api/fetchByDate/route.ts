add1d06aeb1098adbcd9d62849d189334e1fa2eb
mustafa laptop <ad16mkha@uwcad.it>
2025-03-20 07:01:17 -0400

    add pagination

--- src/app/api/fetchByDate/route.ts
+++ src/app/api/fetchByDate/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { off } from 'process';
 
 
 export async function GET(req: Request) {
@@ -10,6 +11,11 @@ export async function GET(req: Request) {
 
     const { searchParams } = new URL(req.url);
     const date = searchParams.get('date');
+    const page = searchParams.get('page');
+    const postsPerPage = 9;
+    const pageNumber = page ? parseInt(page, 10) : 1;
+    const offset: number = (pageNumber - 1) * postsPerPage;
+
     console.log("Gotten query - extracting date");
     console.log("Date:", date);
 
@@ -17,18 +23,20 @@ export async function GET(req: Request) {
       return NextResponse.json({ error: 'Date query parameter is required' }, { status: 400 });
     }
     
-    let { data: analysed_data, error } = await supabase
+    let { data: analysed_data, count, error } = await supabase
     .from('analysed_data')
-    .select('*')
+    .select('*', { count: 'exact' })
     .eq('date_scraped', date)
-    .range(0, 9);
+    .range(offset, offset + postsPerPage - 1);
         
 
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
     
-    return NextResponse.json(analysed_data, { status: 200 });
+    let response = { data: analysed_data, count };
+    return NextResponse.json(response, { status: 200 });
+    
   } catch (error) {
     return NextResponse.json({ error: 'Internal Server Error??' }, { status: 500 });
   }
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -7,6 +7,8 @@ export default function Page() {
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [date, setDate] = useState(getTodayDate());
+  let [currentPage, setCurrentPage] = useState(1);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const today = getTodayDate();
@@ -16,13 +18,13 @@ export default function Page() {
     console.log("gotten here and date is: ", date);
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/fetchByDate?date=${date}`);
+        const response = await fetch(`/api/fetchByDate?date=${date}&page=${currentPage}`);
         const text = await response.text();
-        console.log("Response text:", text);
 
         if (response.ok) {
           const result = JSON.parse(text);
-          setData(result);
+          setData(result.data);
+          setCount(result.count);
         } else {
           console.error("Error fetching data:", text);
         }
@@ -33,12 +35,13 @@ export default function Page() {
     };
 
     fetchData();
-  }, [date]);
+  }, [date, currentPage]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  const totalPages = count ? Math.ceil(count/9) : 0;
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
@@ -63,12 +66,39 @@ export default function Page() {
                             {item.summary}
                           </div>
                           <div>
-                            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                              onClick={() => window.open(item.source_link)}
+                            >
                               View
                             </button>
                           </div>
                   </div>
               ))}
+          </div>
+          <div className="flex justify-center">
+              <nav className="flex space-x-2" aria-label="Pagination">
+                  <a onClick={() => setCurrentPage(1)} className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      First
+                  </a>
+                  <a onClick={() => currentPage==1 ? setCurrentPage(currentPage) : setCurrentPage(currentPage-1)} className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      Previous
+                  </a>
+                  <a  className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      {currentPage - 1}
+                  </a>
+                  <a autoFocus className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      {currentPage}
+                  </a>
+                  <a className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      {currentPage + 1}
+                  </a>
+                  <a onClick={() => currentPage==totalPages ? setCurrentPage(currentPage) : setCurrentPage(currentPage+1)} className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      Next
+                  </a>
+                  <a onClick={() => setCurrentPage(totalPages)} className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                      Last
+                  </a>
+              </nav>
           </div>
         </div>
       </div>
